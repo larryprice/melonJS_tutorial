74e0430ac6a46c0306786c9a7c279e3e947613d0
Larry Price <larry.price.dev@gmail.com>
2013-06-06 21:52:25 -0400

    Modifying tutorial for 'parallax' bg

--- js/resources.js
+++ js/resources.js
@@ -6,6 +6,8 @@ game.resources = [
 	 */
 	 {name: "area01_level_tiles",  type:"image", src: "data/img/map/area01_level_tiles.png"},
 	 {name: "gripe_run_right", type:"image", src: "data/img/sprite/gripe_run_right.png"},
+	 {name: "area01_bkg0",         type:"image", src: "data/img/area01_bkg0.png"},
+   {name: "area01_bkg1",         type:"image", src: "data/img/area01_bkg1.png"},
 	
 	/* Atlases 
 	 * @example
